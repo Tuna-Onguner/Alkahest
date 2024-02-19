92f8663f1fe4bdfaf60ad47794b13af902765e26
Deniz Tuna Onguner <tuna.onguner@ug.bilkent.edu.tr>
2024-02-19 16:09:29 +0300

    Progress bar implemented for sonarqube scanning

--- alkahest/src/models/sonarqube/sonarqube.ts
+++ alkahest/src/models/sonarqube/sonarqube.ts
@@ -1,4 +1,4 @@
-import { window, workspace } from "vscode";
+import { window, workspace, ProgressLocation } from "vscode";
 
 export default class SonarQube {
   private SCToken: any; // The SonarCloud authentication token
@@ -12,49 +12,79 @@ export default class SonarQube {
   }
 
   async scan(): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      const wsfs = workspace.workspaceFolders;
-      if (!wsfs) {
-        window.showErrorMessage("Please open a project before scanning");
-        reject(new Error("No open projects"));
-      } else {
-        const proPath = wsfs[0].uri.fsPath;
+    return window.withProgress(
+      {
+        location: ProgressLocation.Notification,
+        title: "Scanning the project...",
+        cancellable: false,
+      },
+      async (progress) => {
+        let progressValue = 0;
+        progress.report({ increment: 0 });
 
-        const options = { cwd: proPath, stdio: "inherit" };
-        const command = "sonar-scanner";
-        const args = [
-          `-Dsonar.host.url=https://sonarcloud.io/`,
-          `-Dsonar.token=${this.SCToken}`,
-          `-Dsonar.organization=${this.organization}`,
-          `-Dsonar.projectKey=${this.projectKey}`,
-          `-Dsonar.projectName=${wsfs[0].name
-            .replace(/ /g, "_")
-            .toUpperCase()}`,
-          `-Dsonar.sources=${proPath}`,
-        ];
+        try {
+          const wsfs = workspace.workspaceFolders;
+          if (!wsfs) {
+            window.showErrorMessage("Please open a project before scanning");
+            throw new Error("No open projects");
+          }
 
-        const { spawn } = require("child_process");
-        spawn(command, args, options)
-          .on("exit", async (code: number) => {
-            if (code === 0) {
-              window.showInformationMessage("Scanning completed successfully");
+          const proPath = wsfs[0].uri.fsPath;
 
-              const axios = require("axios");
-              const response = await axios.get(
-                `https://sonarcloud.io/api/measures/component?component=${this.projectKey}
-                &metricKeys=bugs,vulnerabilities,code_smells,coverage,duplicated_lines_density`
-              );
+          const options = { cwd: proPath, stdio: "inherit" };
+          const command = "sonar-scanner";
+          const args = [
+            `-Dsonar.host.url=https://sonarcloud.io/`,
+            `-Dsonar.token=${this.SCToken}`,
+            `-Dsonar.organization=${this.organization}`,
+            `-Dsonar.projectKey=${this.projectKey}`,
+            `-Dsonar.projectName=${wsfs[0].name
+              .replace(/ /g, "_")
+              .toUpperCase()}`,
+            `-Dsonar.sources=${proPath}`,
+          ];
 
-              resolve(response.data);
-            } else {
-              window.showErrorMessage("Scanning failed");
-              reject(new Error(`Process exited with code: ${code}`));
+          const { spawn } = require("child_process");
+          const childProcess = spawn(command, args, options);
+
+          const progressInterval = setInterval(() => {
+            if (progressValue < 90) {
+              progressValue += 2;
+              progress.report({ increment: 2 });
             }
-          })
-          .on("error", (error: any) => {
-            reject(error);
+          }, 1550);
+
+          const exitCode = await new Promise<number>((resolve) => {
+            childProcess.on("exit", (code: number) => {
+              clearInterval(progressInterval);
+              resolve(code);
+            });
           });
+
+          if (exitCode === 0) {
+            const axios = require("axios");
+            const response = await axios.get(
+              `https://sonarcloud.io/api/measures/component?component=${this.projectKey}&metricKeys=bugs,vulnerabilities,code_smells,coverage,duplicated_lines_density`
+            );
+
+            progress.report({
+              message: "Scanning completed successfully",
+              increment: 9,
+            });
+
+            await new Promise((resolve) => setTimeout(resolve, 900));
+            progress.report({ increment: 1 });
+
+            return response.data;
+          } else {
+            window.showErrorMessage("Scanning failed");
+            throw new Error(`Process exited with code: ${exitCode}`);
+          }
+        } catch (error: any) {
+          console.error(error.message);
+          throw error;
+        }
       }
-    });
+    );
   }
 }
