0dae4ed9add28986284d8eb628ad8fbf90bc824e
Deniz Tuna Onguner <tuna.onguner@ug.bilkent.edu.tr>
2024-05-11 14:18:46 +0300

    Formatting this branch

--- alkahest/src/views/views/sonarcloud-bsv.ts
+++ alkahest/src/views/views/sonarcloud-bsv.ts
@@ -3,40 +3,46 @@ import * as vscode from "vscode";
 
 export default class SonarCloudBugsSidebarView {
   private static _panel: vscode.WebviewPanel | undefined;
+  private static fullPath = vscode.Uri.file("").fsPath;
 
-  private static fullPath = vscode.Uri.file('').fsPath;
-
-  public static createOrShow(context: vscode.ExtensionContext, bugs: any[]): void {
+  public static createOrShow(
+    context: vscode.ExtensionContext,
+    bugs: any[]
+  ): void {
     const column = vscode.ViewColumn.Two;
 
     if (SonarCloudBugsSidebarView._panel) {
       SonarCloudBugsSidebarView._panel.reveal(column);
     } else {
       SonarCloudBugsSidebarView._panel = vscode.window.createWebviewPanel(
-        'SonarCloudBugsSidebarView',
-        'Bugs Sidebar',
+        "SonarCloudBugsSidebarView",
+        "Bugs Sidebar",
         column,
         {
           enableScripts: true,
         }
       );
 
+      let workspaceRoot;
+
       for (const bug of bugs) {
-        const filePath = bug.component.split(':').slice(2).join(':'); // Extract the file path
+        const filePath = bug.component.split(":").slice(2).join(":"); // Extract the file path
         const decodedFilePath = decodeURIComponent(filePath); // Decode the URL
 
         // Get the workspace root path
-        let workspaceRoot;
-
         if (vscode.workspace.workspaceFolders) {
           workspaceRoot = vscode.workspace.workspaceFolders[0].uri.fsPath;
         } else {
-          console.error('No workspace is open');
+          console.error("No workspace is open");
           return;
         }
 
         // Combine the workspace root path and the file path
-        SonarCloudBugsSidebarView.fullPath = path.join(workspaceRoot, decodedFilePath);
+        SonarCloudBugsSidebarView.fullPath = path.join(
+          workspaceRoot,
+          decodedFilePath
+        );
+
         bug.component = SonarCloudBugsSidebarView.fullPath;
       }
 
@@ -48,14 +54,16 @@ export default class SonarCloudBugsSidebarView {
         context.subscriptions
       );
 
-      SonarCloudBugsSidebarView._panel.webview.html = SonarCloudBugsSidebarView._getWebviewContent(bugs);
+      SonarCloudBugsSidebarView._panel.webview.html =
+        SonarCloudBugsSidebarView._getWebviewContent(bugs);
 
       SonarCloudBugsSidebarView._panel.webview.onDidReceiveMessage(
-        message => {
-          console.log('Received message', message);
-          if (message.command === 'openFile') {
+        (message) => {
+          console.log("Received message", message);
+          if (message.command === "openFile") {
             const openPath = vscode.Uri.file(message.text); // Use the path from the message
-            vscode.workspace.openTextDocument(openPath).then(doc => {
+
+            vscode.workspace.openTextDocument(openPath).then((doc) => {
               vscode.window.showTextDocument(doc); // Open the file
             });
           }
@@ -111,10 +119,15 @@ export default class SonarCloudBugsSidebarView {
 
     // Add a row for each bug
     for (const bug of bugs) {
-        htmlContent += `<tr><td>${bug.id}</td><td>${bug.type}</td><td>${bug.message}</td><td><a href="#" onclick="console.log('Clicked'); vscode.postMessage({ command: 'openFile', text: '${bug.component}' }); return false;">${bug.component}</a></td><td>${bug.line}</td><td>${bug.severity}</td></tr>`;
+      htmlContent += `<tr>
+        <td>${bug.id}</td>
+        <td>${bug.message}</td>
+        <td><a href="#" onclick="console.log('Clicked'); vscode.postMessage({ command: 'openFile', text: '${bug.component}' }); return false;">${bug.component}</a></td>
+        <td>${bug.line}</td><td>${bug.severity}</td>
+      </tr>`;
     }
 
-    htmlContent += '</table></body></html>';
+    htmlContent += "</table></body></html>";
 
     return htmlContent;
   }
